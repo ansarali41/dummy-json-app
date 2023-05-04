1b1620269db64ec65ad9179643ff7bf4db4fb6b5
Zahid <zahid67635gmail.com>
2023-05-04 21:21:07 +0600

    search function correction done

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     const [users, setUsers] = useState([])
     const [showAll, setShowAll] = useState(false);
     const [search, setSearch] = useState(false)
-    const [result, setResult] = useState({})
+    const [filteredData, setFilteredData] = useState({})
     const slicedUsers = showAll ? users : users.slice(0, 10);
     useEffect(() => {
         fetch('https://dummyjson.com/users')
@@ -23,12 +23,19 @@ const Home = () => {
     }
     const handleSubmit = (e) => {
         e.preventDefault()
-        const fname = e.target.user.value
-        setResult({})
-        users.map(user => user?.firstName === fname.charAt(0).toUpperCase() + fname.slice(1).toLowerCase() ? setResult(user) : '')
-        console.log(result)
+        // const fname = e.target.user.value
+        // setResult({})
+        // users.map(user => user?.firstName === fname.charAt(0).toUpperCase() + fname.slice(1).toLowerCase() ? setResult(user) : '')
+        // console.log(result)
+        // setSearch(true)
+        const value = e.target.user.value.toLowerCase();
+        const filteredUsers = users.filter(user => {
+            const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
+            return fullName.includes(value);
+        });
         setSearch(true)
-        e.target.reset()
+
+        setFilteredData(filteredUsers);
     }
     return (
         <div>
@@ -37,29 +44,8 @@ const Home = () => {
                 <input type="text" className="form-control border" placeholder="Type username" aria-label="Recipient's username" aria-describedby="button-addon2" name='user' />
                 <button className="btn btn-outline-secondary ml-1" type="submit" id="button-addon2">Search</button>
             </form>
-            {
-                search ? result.id ? <><h4 className='mt-3'>SEARCH RESULT:</h4>
-                    <table className="table my-3">
-                        <thead>
-                            <tr className='table-warning'>
-                                <th>ID</th>
-                                <th scope="col">First Name</th>
-                                <th scope="col">Last Name</th>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{result.id}</td>
-                                <td>{result.firstName}</td>
-                                <td>{result.lastName}</td>
-                            </tr>
-                        </tbody>
-                    </table></>
-                    : <h3 className='text-center my-2'>User Not Found!!</h3> : ''
-            }
             <div className='mt-4'>
-                <h4>LIST OF USERS: </h4>
+                <h4>LIST OF USERS : </h4>
                 <table className="table">
                     <thead>
                         <tr className='table-warning'>
@@ -71,32 +57,45 @@ const Home = () => {
                     </thead>
                     <tbody>
                         {
-                            slicedUsers?.map((user, i) => <tr key={i}>
-                                <td>
-                                    {user.id}
-                                </td>
-                                <td>{user?.firstName}</td>
-                                <td>{user?.lastName}</td>
+                            search ?
+                                filteredData?.map((user, i) => <tr key={i}>
+                                    <td>
+                                        {user.id}
+                                    </td>
+                                    <td>{user?.firstName}</td>
+                                    <td>{user?.lastName}</td>
 
-                            </tr>)
+                                </tr>)
+                                :
+                                slicedUsers?.map((user, i) => <tr key={i}>
+                                    <td>
+                                        {user.id}
+                                    </td>
+                                    <td>{user?.firstName}</td>
+                                    <td>{user?.lastName}</td>
+
+                                </tr>)
                         }
 
                     </tbody>
 
                 </table>
-                {!showAll ? (
-                    <div>
-                        <div className='d-flex justify-content-center mb-3'>
-                            <div>
-                                <button className='btn btn-outline-secondary' onClick={handleShowAllClick}>Show All</button>
+                {
+                    search ? '' : !showAll ? (
+                        <div>
+                            <div className='d-flex justify-content-center mb-3'>
+                                <div>
+                                    <button className='btn btn-outline-secondary' onClick={handleShowAllClick}>Show All</button>
+                                </div>
                             </div>
                         </div>
+                    ) : <div className='d-flex justify-content-center mb-4'>
+                        <div>
+                            <button className='btn btn-outline-secondary' onClick={handleSliceUser}>Show Less</button>
+                        </div>
                     </div>
-                ) : <div className='d-flex justify-content-center mb-4'>
-                    <div>
-                        <button className='btn btn-outline-secondary' onClick={handleSliceUser}>Show Less</button>
-                    </div>
-                </div>}
+
+                }
             </div>
         </div >
     );
